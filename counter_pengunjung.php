f86f9a0e82a211e1dcf2a87c42c49c4549d78dbd
Ichsan-Ghazali <i43250004@student.polije.ac.id>
2026-04-22 19:28:25 +0700

    after

--- counter_pengunjung.php
+++ counter_pengunjung.php
@@ -8,12 +8,22 @@ class Pengunjung {
         self::$jumlah++;
     }
 
+    public static function reset() {
+        self::$jumlah = 0;
+    }
 }
 
 $p1 = new Pengunjung();
 $p2 = new Pengunjung();
 $p3 = new Pengunjung();
+$p4 = new Pengunjung();
+$p5 = new Pengunjung();
 
-echo "Jumlah Pengunjung: " . Pengunjung::$jumlah;
+echo "Jumlah Pengunjung Sebelum di-reset: " . Pengunjung::$jumlah . "\n";
+echo "<br>";
+
+Pengunjung::reset();
+
+echo "Jumlah Pengunjung Sesudah di-reset: " . Pengunjung::$jumlah . "\n";
 
 ?>
